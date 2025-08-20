300b2d255bc53e55a8e20efb0366c566e72b3ff0
nabeel-ahsan <itsme007nabeel@gmail.com>
2025-08-21 00:19:41 +0530

    Updated css style and js logic

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let outputEl1 = document.getElementById("output-el1")
+let outputEl2 = document.getElementById("output-el2")
+
+function generatePassword(){
+    let passwordOne = randomPassword()
+    let passwordTwo = randomPassword()
+    outputEl1.textContent = passwordOne;
+    outputEl2.textContent = passwordTwo
+
+}
 
 
+function randomPassword() {
+    let randomIndex;
+    let password = ''
+    for(let i = 0; i < 15; i++){
+        randomIndex = Math.floor(Math.random() * 91) 
+        password += characters[randomIndex]
+    }
+    return password
+}
 
